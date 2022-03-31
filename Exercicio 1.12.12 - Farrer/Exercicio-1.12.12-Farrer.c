2b66aa49eedd0a4090d854f9c0a267cef34fb4e7
Rodrigo Eduardo da Silva <98040280+RodrigoEduardo347@users.noreply.github.com>
2022-03-31 09:39:35 -0300

    Update Exercicio-1.12.12-Farrer.c

--- Exercicio 1.12.12 - Farrer/Exercicio-1.12.12-Farrer.c
+++ Exercicio 1.12.12 - Farrer/Exercicio-1.12.12-Farrer.c
@@ -1,3 +1,29 @@
+/*
+
+ 1.12.12. O sistema de avaliação de uma determinada disciplina obedece aos seguintes critérios:
+- durante o semestre são dadas três notas;
+- a nota final é obtida pela média aritmética das notas dadas durante o curso;
+- é considerado aprovado o aluno que obtiver a nota final superior ou igual a 60 e que
+tiver comparecido a um mínimo de 40 aulas.
+Fazer um algoritmo que:
+
+a) Leia um conjunto de dados contendo o número de matrícula, as três notas e a frequência
+(número de aulas frequentadas) de 100 alunos.
+
+b) Calcule:
+- a nota final de cada aluno;
+- a maior e menor nota da turma;
+- a nota média da turma;
+- o total de alunos reprovados; 
+- a porcentagem de alunos reprovados por infrequência;
+
+c) Escreva:
+- para cada aluno, o número de matrícula, a frequência, a nota final e o código
+(aprovado ou reprovado);
+- o que foi calculado no item b (2,3,4 e 5)
+
+*/
+
 #include <stdio.h>
 int main()
 {
@@ -6,7 +32,7 @@ int main()
     float maiorNota, menorNota, contadorNota = 0, mediaTurma, preprofreq;
 
     printf("Entrada de Dados:");
-    for (i = 0; i <= 100; i++)//vai ler 100x
+    for (i = 0; i < 100; i++)//vai ler 100x
     {
         printf("\nNumero da matricula do aluno %d: ", i+1);
         scanf("%f", &matricula[i]);
@@ -20,7 +46,7 @@ int main()
         scanf("%f", &frequencia[i]);
     }
     
-    for (i = 0; i <= 100; i++)
+    for (i = 0; i < 100; i++)
     {
         notaFinal[i] = (nota1[i] + nota2[i] + nota3[i]) / 3;
         contadorNota += notaFinal[i];
@@ -28,7 +54,7 @@ int main()
     mediaTurma = contadorNota / 3;
     
     maiorNota = menorNota = notaFinal[0];
-    for (i = 1; i <= 100; i++)
+    for (i = 1; i < 100; i++)
     {
         if (notaFinal[i] > maiorNota)
         {
@@ -41,7 +67,7 @@ int main()
     }
 
     printf("\n\n\nSaida de Dados:\n");
-    for (i = 0; i <= 100; i++)
+    for (i = 0; i < 100; i++)
     {
         printf("\nNumero da matricula do aluno %d e %.0f: ", i+1, matricula[i]);
         printf("\nNumero de aulas que o aluno %d assistiu e %.2f ", i+1, frequencia[i]);
